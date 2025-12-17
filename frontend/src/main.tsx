c72953c41db96421afee4d97b6a1ec9420bc0fdc
Douglas Yuji Yabuki <douglasyabuki@gmail.com>
2025-12-17 13:27:27 -0300

    Add: GraphQL mutations (login and register) & configure Auth store & Apollo Client

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from "react";
+import { ApolloProvider } from "@apollo/client/react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import { App } from "./App.tsx";
 import "./index.css";
+import { apolloClient } from "./lib/graphql/apollo.ts";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <ApolloProvider client={apolloClient}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ApolloProvider>,
 );
