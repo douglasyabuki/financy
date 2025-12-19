e850915684d9493290b2e75a0603cea463cdeb4a
Douglas Yuji Yabuki <douglasyabuki@gmail.com>
2025-12-19 16:31:21 -0300

    Add: Dialogs to edit and delete a category

---
+++ frontend/src/components/categories/delete-category-dialog.tsx
@@ -0,0 +1,81 @@
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { DELETE_CATEGORY } from "@/lib/graphql/mutations/category";
+import type { Category } from "@/types";
+import { useMutation } from "@apollo/client/react";
+import { Tag } from "../ui/tag";
+
+interface DeleteCategoryDialog {
+  isOpen: boolean;
+  onClose: () => void;
+  category: Category | null;
+  onDeleted?: () => void;
+}
+
+export const DeleteCategoryDialog = ({
+  isOpen,
+  onClose,
+  category,
+  onDeleted,
+}: DeleteCategoryDialog) => {
+  type DeleteCategoryMutationData = { deleteCategory: boolean };
+  type DeleteCategoryVariables = { id: string };
+
+  const [deleteCategoryMutation, { loading }] = useMutation<
+    DeleteCategoryMutationData,
+    DeleteCategoryVariables
+  >(DELETE_CATEGORY, {
+    onCompleted: () => {
+      onDeleted?.();
+      onClose();
+    },
+  });
+
+  const handleDelete = async () => {
+    if (!category) return;
+    await deleteCategoryMutation({
+      variables: {
+        id: category.id,
+      },
+    });
+  };
+
+  return (
+    <Dialog open={isOpen} onOpenChange={onClose}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle className="text-lg leading-6 font-semibold tracking-normal text-gray-800">
+            Deletar categoria
+          </DialogTitle>
+          <DialogDescription className="text-sm leading-5 font-normal tracking-normal text-gray-600">
+            Tem certeza que deseja deletar a categoria?
+          </DialogDescription>
+        </DialogHeader>
+        <div className="space-y-2">
+          <p className="text-sm font-medium">Categoria:</p>
+          <Tag color={category?.color}>{category?.title}</Tag>
+        </div>
+        <DialogFooter>
+          <Button variant="outline" size="custom-sm" onClick={onClose}>
+            Cancelar
+          </Button>
+          <Button
+            size="custom-sm"
+            variant="destructive"
+            onClick={handleDelete}
+            disabled={loading}
+          >
+            {loading ? "Deletando..." : "Deletar"}
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
---
+++ frontend/src/components/categories/edit-category-dialog.tsx
@@ -0,0 +1,224 @@
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { Label } from "@/components/ui/label";
+import { type CategoryColor, type CategoryIcon } from "@/constants/categories";
+import { UPDATE_CATEGORY } from "@/lib/graphql/mutations/category";
+import { cn } from "@/lib/utils";
+import type { Category } from "@/types";
+import { useMutation } from "@apollo/client/react";
+import { useEffect } from "react";
+import { Controller, useForm } from "react-hook-form";
+import { toast } from "sonner";
+import { InputGroup, InputGroupInput, InputGroupText } from "../ui/input-group";
+import { ColorPicker } from "./color-picker";
+import { IconPicker } from "./icon-picker";
+
+interface EditCategoryDialog {
+  isOpen: boolean;
+  onClose: () => void;
+  category: Category | null;
+  onUpdated?: () => void;
+}
+
+export const EditCategoryDialog = ({
+  isOpen,
+  onClose,
+  category,
+  onUpdated,
+}: EditCategoryDialog) => {
+  const {
+    control,
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<{
+    title: string;
+    description: string;
+    icon: CategoryIcon;
+    color: CategoryColor;
+  }>({
+    defaultValues: {
+      title: "",
+      description: "",
+      icon: "BRIEFCASE_BUSINESS",
+      color: "GREEN",
+    },
+  });
+
+  useEffect(() => {
+    if (isOpen && category) {
+      reset({
+        title: category.title,
+        description: category.description || "",
+        icon: category.icon as CategoryIcon,
+        color: category.color as CategoryColor,
+      });
+    }
+  }, [isOpen, category, reset]);
+
+  const [updateCategory, { loading }] = useMutation(UPDATE_CATEGORY, {
+    onCompleted() {
+      toast.success("Categoria atualizada com sucesso");
+      onClose();
+      onUpdated?.();
+    },
+    onError(err) {
+      console.log(err);
+      toast.error("Falha ao atualizar categoria");
+    },
+  });
+
+  const onSubmit = (data: {
+    title: string;
+    description: string;
+    icon: CategoryIcon;
+    color: CategoryColor;
+  }) => {
+    if (!category) return;
+
+    updateCategory({
+      variables: {
+        id: category.id,
+        data: {
+          title: data.title,
+          description: data.description,
+          icon: data.icon,
+          color: data.color,
+        },
+      },
+    });
+  };
+
+  return (
+    <Dialog open={isOpen} onOpenChange={onClose}>
+      <DialogContent className="w-md gap-6">
+        <DialogHeader className="gap-0.5">
+          <DialogTitle className="text-lg leading-6 font-semibold tracking-normal text-gray-800">
+            Editar categoria
+          </DialogTitle>
+          <DialogDescription className="text-sm leading-5 font-normal tracking-normal text-gray-600">
+            Organize suas transações com categorias
+          </DialogDescription>
+        </DialogHeader>
+        <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+          <div className="group/title space-y-2">
+            <Label
+              htmlFor="title"
+              className={cn(
+                "group-has-[[data-slot][aria-invalid=true]]/title:text-destructive",
+                "group-has-active/title:text-primary group-has-focus/title:text-primary",
+              )}
+            >
+              Título
+            </Label>
+            <InputGroup className="h-12">
+              <InputGroupInput
+                className="text-base leading-4.5 tracking-normal text-gray-800 placeholder:text-base placeholder:leading-4.5 placeholder:tracking-normal placeholder:text-gray-400"
+                id="title"
+                placeholder="Ex: Alimentação"
+                aria-invalid={!!errors.title}
+                {...register("title", {
+                  required: "Título é obrigatório",
+                  minLength: {
+                    value: 3,
+                    message: "Título deve ter no mínimo 3 caracteres",
+                  },
+                  validate: (value) =>
+                    value.trim().length > 0 || "Título não pode ser vazio",
+                })}
+              />
+            </InputGroup>
+            {errors.title && (
+              <InputGroupText>{errors.title.message as string}</InputGroupText>
+            )}
+          </div>
+          <div className="group/description space-y-2">
+            <Label
+              htmlFor="description"
+              className={cn(
+                "group-has-[[data-slot][aria-invalid=true]]/description:text-destructive",
+                "group-has-active/description:text-primary group-has-focus/description:text-primary",
+              )}
+            >
+              Descrição
+            </Label>
+            <InputGroup className="h-12">
+              <InputGroupInput
+                className="text-base leading-4.5 tracking-normal text-gray-800 placeholder:text-base placeholder:leading-4.5 placeholder:tracking-normal placeholder:text-gray-400"
+                id="description"
+                type="description"
+                placeholder="Descrição da categoria"
+                autoComplete="description"
+                {...register("description")}
+              />
+            </InputGroup>
+            <InputGroupText>Opcional</InputGroupText>
+          </div>
+          <div className="space-y-2">
+            <Label
+              className={cn(errors.icon && "text-destructive")}
+              aria-invalid={!!errors.icon}
+            >
+              Ícone
+            </Label>
+
+            <Controller
+              control={control}
+              name="icon"
+              rules={{ required: "Ícone é obrigatório" }}
+              render={({ field }) => (
+                <IconPicker
+                  icon={field.value}
+                  onIconChange={(nextIcon: string) => field.onChange(nextIcon)}
+                />
+              )}
+            />
+            {errors.icon && (
+              <InputGroupText>{errors.icon.message as string}</InputGroupText>
+            )}
+          </div>
+          <div className="space-y-2">
+            <Label
+              className={cn(errors.icon && "text-destructive")}
+              aria-invalid={!!errors.icon}
+            >
+              Cor
+            </Label>
+            <Controller
+              control={control}
+              name="color"
+              rules={{ required: "Cor é obrigatória" }}
+              render={({ field }) => (
+                <ColorPicker
+                  color={field.value}
+                  onColorChange={(nextColor: CategoryColor) =>
+                    field.onChange(nextColor)
+                  }
+                />
+              )}
+            />
+            {errors.color && (
+              <InputGroupText>{errors.color.message as string}</InputGroupText>
+            )}
+          </div>
+          <Button
+            className="w-full"
+            type="submit"
+            disabled={loading}
+            variant="custom-primary"
+            size="custom-md"
+          >
+            Salvar
+          </Button>
+        </form>
+      </DialogContent>
+    </Dialog>
+  );
+};
